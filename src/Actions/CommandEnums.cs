591f516b4181245e1f4c140e6ce684fd33c7ea3d
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-12-11 14:38:23 +0900

    refactor: move handler

---
+++ src/Actions/CommandEnums.cs
@@ -0,0 +1,15 @@
+namespace Actions;
+
+public enum VersionIncrement
+{
+    Major,
+    Minor,
+    Patch,
+    //Prerelease, // TODO: how to calculate count since last tag?
+}
+
+public enum OutputFormatType
+{
+    Console,
+    GitHubActions,
+}
---
+++ src/Actions/Handlers/VersionHandler.cs
@@ -0,0 +1,67 @@
+namespace Actions.Handlers;
+
+public class VersionHandler(string tag, string prefix, VersionIncrement versionIncrement, bool isPrelease, string prerelease)
+{
+    /// <summary>
+    /// Handling versioning
+    /// </summary>
+    /// <returns></returns>
+    public string Versioning()
+    {
+        var version = GetNormalizedVersion(tag, prefix);
+        var increment = IncrementVersion(version, versionIncrement);
+        var format = FormatVersion(increment, prefix, isPrelease, prerelease);
+        return format;
+    }
+
+    /// <summary>
+    /// Normalize tag. Tag may contains prefix, remove prefix and retrun version.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <param name="prefix"></param>
+    /// <returns></returns>
+    /// <exception cref="ActionCommandException"></exception>
+    private static Version GetNormalizedVersion(string tag, string prefix)
+    {
+        if (string.IsNullOrEmpty(tag)) throw new ActionCommandException("tag missing");
+        if (tag.StartsWith(prefix, StringComparison.Ordinal))
+        {
+            var span = tag.AsSpan();
+            var substring = span[prefix.Length..];
+            return Version.Parse(substring);
+        }
+        else
+        {
+            return Version.Parse(tag);
+        }
+    }
+
+    /// <summary>
+    /// Increment version for specific place
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="versionIncrement"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    private static Version IncrementVersion(Version version, VersionIncrement versionIncrement) => versionIncrement switch
+    {
+        VersionIncrement.Major => new Version(version.Major + 1, version.Minor, version.Build),
+        VersionIncrement.Minor => new Version(version.Major, version.Minor + 1, version.Build),
+        VersionIncrement.Patch => new Version(version.Major, version.Minor, version.Build + 1),
+        _ => throw new NotImplementedException(nameof(versionIncrement)),
+    };
+
+    /// <summary>
+    /// Format version
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="prefix"></param>
+    /// <param name="isPrelease"></param>
+    /// <param name="prerelease"></param>
+    /// <returns></returns>
+    private static string FormatVersion(Version version, string prefix, bool isPrelease, string prerelease)
+    {
+        var preReleaseSuffix = isPrelease ? $"-{prerelease}" : "";
+        return $"{prefix}{version}{preReleaseSuffix}";
+    }
+}
