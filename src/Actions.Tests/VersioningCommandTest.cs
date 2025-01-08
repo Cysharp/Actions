0f2994e10402c5e9fc69dae1a055a1d93df194a3
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-01-08 20:23:34 +0900

    feat: move validate-tag to zx

--- src/Actions.Tests/VersioningCommandTest.cs
+++ src/Actions.Tests/VersioningCommandTest.cs
@@ -9,12 +9,12 @@ public class VersioningCommandTest
     [InlineData("0.1.0", VersionIncrement.Major, "1.1.0")]
     [InlineData("0.1.0", VersionIncrement.Minor, "0.2.0")]
     [InlineData("0.1.0", VersionIncrement.Patch, "0.1.1")]
-    public void VersionIncrementTest(string tag, VersionIncrement versionIncrement, string actual)
+    public void VersionIncrementTest(string tag, VersionIncrement versionIncrement, string expected)
     {
         var command = new VersioningCommand(tag, prefix: "", versionIncrement: versionIncrement, isPrelease: false, prerelease: "");
         var versioning = command.Versioning();
 
-        versioning.Should().Be(actual);
+        versioning.Should().Be(expected);
     }
 
     [Theory]
@@ -24,23 +24,23 @@ public class VersioningCommandTest
     [InlineData("v0.1.0", "v", false, "v0.1.1")]
     [InlineData("Ver0.1.0", "Ver", false, "Ver0.1.1")]
     [InlineData("Ver.0.1.0", "Ver.", false, "Ver.0.1.1")]
-    public void VersionPrefixTest(string tag, string prefix, bool withoutPrefix, string actual)
+    public void VersionPrefixTest(string tag, string prefix, bool withoutPrefix, string expected)
     {
         var command = new VersioningCommand(tag, prefix: prefix, versionIncrement: VersionIncrement.Patch, isPrelease: false, prerelease: "");
         var versioning = command.Versioning(withoutPrefix);
 
-        versioning.Should().Be(actual);
+        versioning.Should().Be(expected);
     }
 
     [Theory]
     [InlineData("0.1.0", "", "0.1.1")]
     [InlineData("0.1.0", "alpha", "0.1.1-alpha")]
     [InlineData("0.1.0", "preview", "0.1.1-preview")]
-    public void VersionPrereleaseTest(string tag, string prerelease, string actual)
+    public void VersionPrereleaseTest(string tag, string prerelease, string expected)
     {
         var command = new VersioningCommand(tag, prefix: "", versionIncrement: VersionIncrement.Patch, isPrelease: true, prerelease: prerelease);
         var versioning = command.Versioning();
 
-        versioning.Should().Be(actual);
+        versioning.Should().Be(expected);
     }
 }
---
+++ src/Actions/Commands/ValidateTagCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Actions.Commands;
+
+public class ValidateTagCommand()
+{
+    /// <summary>
+    /// Normalize input tag. If the tag starts with 'v', it will be removed.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public string Normalize(string tag) => tag.StartsWith("v") ? tag.Substring(1) : tag;
+
+    /// <summary>
+    /// Validate input tag. If the input tag is older than the latest release tag, it will return false. Otherwise, it will return true.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public async Task<(bool result, string releaseTag)> ValidateTagAsync(string tag)
+    {
+        // release_latest=$(gh release list --exclude-drafts --exclude-pre-releases --json tagName,isLatest | jq -c -r ".[] | select(.isLatest == true) | .tagName")
+        // sorted_latest=$(echo - e "${release_latest}\n${{ steps.trim.outputs.normalized_tag }}" | sort - V | tail - n 1)
+        var releaseLatests = await "gh release list --exclude-drafts --exclude-pre-releases --json tagName,isLatest";
+        var githubReleases = JsonSerializer.Deserialize<GitHubRelease[]>(releaseLatests);
+        var releaseTag = githubReleases?.SingleOrDefault(x => x.IsLatest)?.TagName;
+
+        if (releaseTag == null)
+        {
+            // no release tag
+            return (true, "");
+        }
+
+        var sortedLatest = new[] { releaseTag, tag }.OrderBy(x => x).Last();
+        if (sortedLatest == tag)
+        {
+            // input tag is same or newer than latest tag
+            return (true, releaseTag);
+        }
+
+        // input tag is older than latest tag, reverting!!
+        return (false, releaseTag);
+    }
+
+    private record GitHubRelease
+    {
+        [JsonPropertyName("tagName")]
+        public required string TagName { get; init; }
+        [JsonPropertyName("isLatest")]
+        public required bool IsLatest { get; init; }
+    }
+}
