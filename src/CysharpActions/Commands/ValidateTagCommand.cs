adfa69518150c09b25bb0bf3c638b0f1913b664e
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-06-03 12:27:12 +0900

    chore: 1.0.9対して1.0.10が新しいと判定されないテストを追加

--- src/CysharpActions/Commands/ValidateTagCommand.cs
+++ src/CysharpActions/Commands/ValidateTagCommand.cs
@@ -5,7 +5,30 @@ using System.Text.Json;
 
 namespace CysharpActions.Commands;
 
-public class ValidateTagCommand()
+/// <summary>
+/// GitHubのリリース情報を取得するためのインターフェース。
+/// </summary>
+public interface IGitHubRelaeseExe
+{
+    public Task<GitHubRelease[]> GetGitHubReleaseAsync();
+}
+
+/// <summary>
+/// ghコマンドでGitHubのリリース情報を取得する実装。
+/// </summary>
+public class GitHubReleaseExeGh : IGitHubRelaeseExe
+{
+    public async Task<GitHubRelease[]> GetGitHubReleaseAsync()
+    {
+        // release_latest=$(gh release list --exclude-drafts --exclude-pre-releases --json tagName,isLatest | jq -c -r ".[] | select(.isLatest == true) | .tagName")
+        // sorted_latest=$(echo - e "${release_latest}\n${{ steps.trim.outputs.normalized_tag }}" | sort - V | tail - n 1)
+        var releaseLatests = await "gh release list --exclude-drafts --exclude-pre-releases --json tagName,isLatest";
+        var githubReleases = JsonSerializer.Deserialize(releaseLatests, JsonSourceGenerationContext.Default.GitHubReleaseArray);
+        return githubReleases ?? Array.Empty<GitHubRelease>();
+    }
+}
+
+public class ValidateTagCommand(IGitHubRelaeseExe gitHubRelaeseExe)
 {
     /// <summary>
     /// Normalize input tag. If the tag starts with 'v', it will be removed.
@@ -35,10 +58,7 @@ public class ValidateTagCommand()
 
         try
         {
-            // release_latest=$(gh release list --exclude-drafts --exclude-pre-releases --json tagName,isLatest | jq -c -r ".[] | select(.isLatest == true) | .tagName")
-            // sorted_latest=$(echo - e "${release_latest}\n${{ steps.trim.outputs.normalized_tag }}" | sort - V | tail - n 1)
-            var releaseLatests = await "gh release list --exclude-drafts --exclude-pre-releases --json tagName,isLatest";
-            var githubReleases = JsonSerializer.Deserialize(releaseLatests, JsonSourceGenerationContext.Default.GitHubReleaseArray);
+            var githubReleases = await gitHubRelaeseExe.GetGitHubReleaseAsync();
             var releaseTag = githubReleases?.SingleOrDefault(x => x.IsLatest)?.TagName;
 
             if (releaseTag is null)
